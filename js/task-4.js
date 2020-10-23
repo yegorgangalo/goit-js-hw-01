86122049ff5c43e14974d2d652617dd14c98b63d
Yegor Gangalo <gangaloyegor@gmail.com>
2020-10-23 14:26:00 +0300

    made task 5

--- js/task-4.js
+++ js/task-4.js
@@ -1,24 +1,24 @@
-const credits = 23580;
-const pricePerDroid = 3000;
-let message;
-let totalPrice;
-const quantity = prompt("введите количество дронов");
-const quantityNumber = Number(quantity);
-if (quantity === null) {
-    message = 'Отменено пользователем!';
-}
-else if (quantityNumber != NaN && quantityNumber > 0) {
-    totalPrice = pricePerDroid * Number(quantity);
-    if (totalPrice > credits) {
-        message = 'Недостаточно средств на счету!';
-    }
-    else {
-        const balance = credits - totalPrice;
-        message = `Вы купили ${quantityNumber} дроидов, на счету осталось ${balance} кредитов.`;
-    }
-}
-else {
-    message = "Введите количество от 1";
-}
+// const credits = 23580;
+// const pricePerDroid = 3000;
+// let message;
+// let totalPrice;
+// const quantity = prompt("введите количество дронов");
+// const quantityNumber = Number(quantity);
+// if (quantity === null) {
+//     message = 'Отменено пользователем!';
+// }
+// else if (quantityNumber != NaN && quantityNumber > 0) {
+//     totalPrice = pricePerDroid * Number(quantity);
+//     if (totalPrice > credits) {
+//         message = 'Недостаточно средств на счету!';
+//     }
+//     else {
+//         const balance = credits - totalPrice;
+//         message = `Вы купили ${quantityNumber} дроидов, на счету осталось ${balance} кредитов.`;
+//     }
+// }
+// else {
+//     message = "Введите количество от 1";
+// }
 
-console.log(message);
+// console.log(message);
